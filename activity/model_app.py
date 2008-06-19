30b4237efe272e88ff9ec41cc56c26c1ed1db702
Bobby Powers <bobby@laptop.org>
2008-06-19 13:52:45 -0400

    activity starts up, again

--- activity/model_app.py
+++ activity/model_app.py
@@ -2,44 +2,11 @@ import pygtk
 pygtk.require("2.0")
 
 from sugar.activity import activity
-from sugar.graphics.toolbutton import ToolButton
 
-import gtk, math
+import math
 from gettext import gettext as _
 
 import opensim as sim
-
-
-class ModelToolbar(gtk.Toolbar):
-  """Provides the toolbar containing the basic modeling functions"""
-
-  def __init__(self):
-    gtk.Toolbar.__init__(self)
-
-    #Get our 4 buttons & add type attributes to them
-    self.variable = ToolButton("icon-var")
-    self.variable.set_tooltip(_('Create Variables'))
-    self.insert(self.variable, -1)
-    self.variable.show()
-    self.variable.type = sim.VARIABLE
-
-    self.influence = ToolButton("icon-infl")
-    self.influence.set_tooltip(_('Create Influence Arrows'))
-    self.insert(self.influence, -1)
-    self.influence.show()
-    self.influence.type = sim.INFLUENCE
-
-    self.stock = ToolButton("icon-stock")
-    self.stock.set_tooltip(_('Create Stocks'))
-    self.insert(self.stock, -1)
-    self.stock.show()
-    self.stock.type = sim.STOCK
-
-    self.flow = ToolButton("icon-flow")
-    self.flow.set_tooltip(_('Create Flows'))
-    self.insert(self.flow, -1)
-    self.flow.show()
-    self.flow.type = sim.FLOW    
 
 
 class ModelActivity(activity.Activity):
@@ -66,9 +33,13 @@ class ModelActivity(activity.Activity):
     # functionalities, such as the 'Collaborate' and 'Close' buttons.
     toolbox = activity.ActivityToolbox(self)
 
-    toolbar = activity.EditToolbar()
-    toolbox.add_toolbar(_('Edit'), toolbar)
-    toolbar.show()
+    self.edit_toolbar = activity.EditToolbar()
+    toolbox.add_toolbar(_('Edit'), self.edit_toolbar)
+    self.edit_toolbar.show()
+
+    self.model_toolbar = sim.ModelToolbar()
+    toolbox.add_toolbar(_('Model'), self.model_toolbar)
+    self.model_toolbar.show()
 
     #self._edit_toolbar = activity.EditToolbar(self.canvas)
     #toolbox.add_toolbar(_('Edit'), self._edit_toolbar)
--- lib/python/__init__.py
+++ lib/python/__init__.py
@@ -1,3 +1,45 @@
 import simulator
-from canvas import Canvas
 from constants import *
+
+from gettext import gettext as _
+
+import pygtk
+pygtk.require("2.0")
+
+from canvas import Canvas
+
+import gtk
+from sugar.graphics.toolbutton import ToolButton
+
+class ModelToolbar(gtk.Toolbar):
+  """Provides the toolbar containing the basic modeling functions"""
+
+  def __init__(self):
+    gtk.Toolbar.__init__(self)
+
+    #Get our 4 buttons & add type attributes to them
+    self.stock = ToolButton("icon-stock")
+    self.stock.set_tooltip(_('Create Stocks'))
+    self.insert(self.stock, -1)
+    self.stock.show()
+    self.stock.type = STOCK
+
+    self.flow = ToolButton("icon-flow")
+    self.flow.set_tooltip(_('Create Flows'))
+    self.insert(self.flow, -1)
+    self.flow.show()
+    self.flow.type = FLOW    
+
+    self.variable = ToolButton("icon-var")
+    self.variable.set_tooltip(_('Create Variables'))
+    self.insert(self.variable, -1)
+    self.variable.show()
+    self.variable.type = VARIABLE
+
+    self.influence = ToolButton("icon-infl")
+    self.influence.set_tooltip(_('Create Influence Arrows'))
+    self.insert(self.influence, -1)
+    self.influence.show()
+    self.influence.type = INFLUENCE
+
+
