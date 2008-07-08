38cf78f2cae02efd7e674a0db159e364bfea1c63
Bobby Powers <bobby@laptop.org>
2008-07-07 23:28:55 -0400

    Bundle release 7

--- activity/model_app.py
+++ activity/model_app.py
@@ -46,8 +46,6 @@ class ModelActivity(activity.Activity):
       # make sure we catch the case where no tool is active.
       if widget.type == self.canvas.get_active_tool():
         self.canvas.set_active_tool(sim.NONE)
-        if self.canvas.goocanvas.highlighted:
-          self.canvas.goocanvas.highlighted.emit("highlight_out_event", self)
       return
 
     # switch between possible widgets.
