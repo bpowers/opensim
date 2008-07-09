d850fbc60846e0e39a520b3fadd5965321063d01
Bobby Powers <bobby@laptop.org>
2008-07-09 18:02:27 -0400

    revert consolidation of functions - broke text alignment

--- lib/python/visuals/widgets/stock.py
+++ lib/python/visuals/widgets/stock.py
@@ -32,10 +32,10 @@ import cairo
 import logging
 
 from text import TextInfo
-from independent import IndependentItem
+from item import SimItem
 
 
-class StockItem(IndependentItem):
+class StockItem(SimItem):
 
   def __init__(self, x, y, width=140, height=80, name=None,
                focus=True, line_width=3.5, **kwargs):
@@ -80,6 +80,10 @@ class StockItem(IndependentItem):
                  self.y - self.line_width/2.0,
                  self.width + self.line_width/2.0, 
                  self.height + self.line_width/2.0)
+
+
+  def center(self):
+    return (int(self.x + self.width/2), int(self.y + self.height/2))
 
 
   def ensure_size(self, cr):
@@ -144,6 +148,109 @@ class StockItem(IndependentItem):
     return xml_string
 
 
+  def name(self):
+    return self._display_name.string
+
+
+  def on_key_press(self, item, target, event):
+    key_name = gtk.gdk.keyval_name(event.keyval)
+
+    if key_name in self.enter_key:
+      self.emit("highlight_out_event", self)
+    elif key_name in self.delete_key:
+      self._display_name.backspace()
+    elif key_name in self.escape_key:
+      print("escape key!")
+    else:
+      # add key to name buffer
+      self._display_name.add(event.string)
+
+    self.__needs_resize_calc = True
+    self.force_redraw()
+
+    # return true to stop propogation
+    return True
+
+
+  def on_button_press(self, item, target, event):
+    canvas = self.get_canvas()
+
+    if canvas.override:
+      # if we're in the process of drawing a line, just 
+      # propogate the signal.
+      return False
+
+    canvas.grab_focus(item)
+    canvas.grab_highlight(self)
+
+    if event.button == 1:
+      self.drag_x = event.x
+      self.drag_y = event.y
+
+      fleur = gtk.gdk.Cursor(gtk.gdk.FLEUR)
+      canvas = item.get_canvas()
+      canvas.pointer_grab(item,
+                          gtk.gdk.POINTER_MOTION_MASK 
+                           | gtk.gdk.BUTTON_RELEASE_MASK,
+                          fleur, event.time)
+      self.dragging = True
+    elif event.button == 3:
+      # right-click, handle later
+      pass
+    else:
+      print "unsupported button: %d" % event.button
+    return True
+
+
+  def on_button_release(self, item, target, event):
+    canvas = item.get_canvas()
+    canvas.pointer_ungrab(item, event.time)
+    self.dragging = False
+
+
+  def on_motion_notify (self, item, target, event):
+    if (self.dragging == True) and (event.state & gtk.gdk.BUTTON1_MASK):
+      new_x = event.x
+      new_y = event.y
+      item.translate(new_x - self.drag_x, new_y - self.drag_y)
+      return True
+    return False
+
+
+  def on_focus_in(self, item, target, event):
+    return False
+
+
+  def on_focus_out(self, item, target, event):
+    return False
+
+
+  def on_highlight_in(self, item, target):
+    self.text_color = [1, .6, .2]
+    self.force_redraw()
+
+    return False
+
+
+  def on_highlight_out(self, item, target):
+    self.text_color = [0, 0, 0]
+    self.force_redraw()
+
+    if self._new:
+      if self._display_name.placeholder:
+        self.get_canvas().remove_item(self)
+        return
+
+    self.get_canvas().update_name(self._display_name.string, 
+                                  self, new=self._new)
+
+    self._new = False
+
+    return False
+
+
+  
+
+
 
 gobject.type_register(StockItem)
-
