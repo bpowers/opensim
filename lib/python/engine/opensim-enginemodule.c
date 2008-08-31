b574920ffc8f931f86b55d36b8c724e90ede1ee5
Bobby Powers <bobby@laptop.org>
2008-08-31 12:36:21 -0400

    work on getting both var and sim in python bindings (UNFINISHED)

---
+++ lib/python/engine/opensim-enginemodule.c
@@ -0,0 +1,22 @@
+#include <pygobject.h>
+ 
+void engine_register_classes (PyObject *d); 
+extern PyMethodDef engine_functions[];
+ 
+DL_EXPORT(void)
+initengine(void)
+{
+    PyObject *m, *d;
+ 
+    init_pygobject ();
+ 
+    m = Py_InitModule ("opensim.engine", engine_functions);
+    d = PyModule_GetDict (m);
+ 
+    engine_register_classes (d);
+ 
+    if (PyErr_Occurred ()) {
+        Py_FatalError ("can't initialise module engine");
+    }
+}
+
