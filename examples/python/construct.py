cf1d1f8ab74954d38705724767ad4c39b66ca867
Bobby Powers <bobby@laptop.org>
2008-09-21 18:35:46 -0400

    clean up

--- examples/python/construct.py
+++ examples/python/construct.py
@@ -11,6 +11,8 @@ def save_extra(sim, save_file_pointer):
   else:
     print "crap, returned NULL"
 
+
+
 def run ():
   sim = engine.Simulator ()
   
@@ -23,16 +25,13 @@ def run ():
   sim.new_variable ('lookup', '[(0,2),(50,40),(80,56)]')
   sim.new_variable ('test', 'lookup[time]')
 
-  #print 'sim membs: ', list(engine.Simulator.__dict__)
-  #print 'sim props: ', list(engine.Simulator.props)
-  #print 'currently: ', sim.get_property('output-type')
   sim.props.output_type =  engine.emit_Python
-  #sim.run()
+
+  sim.run()
+
 
   sim.connect ("saving", save_extra)
-
-
-  #sim.props.file_name = "test_sim.osm"
+  sim.props.file_name = "test_sim.osm"
   sim.save ()
 
 if __name__ == '__main__':
