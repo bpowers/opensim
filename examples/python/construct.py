86da9bbb2bfcf6bde50881284dcfe29cf702c618
Bobby Powers <bobby@laptop.org>
2008-10-15 17:36:45 -0400

    fixed whitespace in python example

--- examples/python/construct.py
+++ examples/python/construct.py
@@ -9,6 +9,7 @@ def equation_checker (var, old_equation):
   print "  old equation: '%s'" % old_equation
   print "  new equation: '%s'\n" % var.props.equation
 
+
 # callback function that is executed when sim.save () is called
 def save_extra (sim, save_file_pointer):
   save_file = engine.get_file (sim, save_file_pointer)
@@ -26,18 +27,18 @@ def run ():
   sim.new_variable ('OS_end', '100')
   sim.new_variable ('OS_timestep', '.25')
   sim.new_variable ('OS_savestep', '4')
-
+  
   # set sim-specific variables  
   sim.new_variable ('weird', '3.14*time')
   sim.new_variable ('lookup', '[(0,2),(50,40),(80,56)]')
   sim.new_variable ('test', 'lookup[time]')
   sim.new_variable ('blank', '')
-
+  
   test = sim.get_variable ('test')
   if test is not None:
     test.connect ("equation_changed", equation_checker)
     test.props.equation = 'lookup[time] * weird'
-
+  
   # set our output to be python, and call run.  since we haven't specified
   # an output file-name, it defaults to standard output.
   sim.props.output_type =  engine.emit_Python
