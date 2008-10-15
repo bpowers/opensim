3a8b93cc5e71286fdb801692affb31f0ae2cdbfb
Bobby Powers <bobby@laptop.org>
2008-10-15 11:35:39 -0400

    update python example to reflect cleaner new_variable api

--- examples/python/construct.py
+++ examples/python/construct.py
@@ -32,9 +32,8 @@ def run ():
   sim.new_variable ('weird', '3.14*time')
   sim.new_variable ('lookup', '[(0,2),(50,40),(80,56)]')
   sim.new_variable ('test', 'lookup[time]')
-  sim.new_variable ('blank', '')
+  test = sim.new_variable ('blank', '')
   
-  test = sim.get_variable ('test')
   if test is not None:
     test.connect ("equation_changed", equation_checker)
     test.props.equation = 'lookup[time] * weird'
