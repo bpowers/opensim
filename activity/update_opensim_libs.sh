ef5779990f251b61d7d5eda96a8ee714cdc5846e
Bobby Powers <bobby@laptop.org>
2008-10-18 22:05:14 -0400

    updated update_opensim_libs to correctly rename engine.so

--- activity/update_opensim_libs.sh
+++ activity/update_opensim_libs.sh
@@ -7,6 +7,9 @@ export GROUP=`id -gn`
 sudo rm -rf site-packages/opensim
 sudo cp -R /usr/lib/python2.5/site-packages/opensim site-packages/
 sudo chown -R $USER:$GROUP site-packages
+rm site-packages/opensim/engine.so
+rm site-packages/opensim/engine.so.0
+mv site-packages/opensim/engine.so.0.0.0 site-packages/opensim/engine.so
 
 #update opensim lib
 sudo rm -rf lib/libsim.so.0
