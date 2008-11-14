becfa09c802f184e14ed6109d436ddbb5d07a17a
Bobby Powers <bobby@laptop.org>
2008-11-13 20:12:25 -0500

    [mac] working, automated app building on the mac

--- mac/update_model_libs.sh
+++ mac/update_model_libs.sh
@@ -6,23 +6,31 @@ export USER=root
 export GROUP=admin
 export SRC_DIR=`pwd`/..
 
-# make the bundle easier to work with
+echo "making the bundle easier to work with"
 sudo chown -R $CUR_USER:$CUR_GROUP Model.app
 
 cd Model.app/Contents/Resources
 
-# update python libs
+echo "updating python libs"
 rm -rf ./opensim
 cp -R /Library/Python/2.5/site-packages/opensim ./
 
 # requires libs be built with -headerpad_max_install_names
-install_name_tool ./opensim/engine.so -change /usr/lib/libsim.0.dylib \
-                  @executable_path/../Resources/lib/libsim.0.dylib
+echo "fixing engine's mach-o loads"
+install_name_tool -change /usr/lib/libsim.0.dylib \
+                  @executable_path/../Resources/lib/libsim.0.dylib \
+                  ./opensim/engine.so
 
-rm -f ./lib/*
+echo "updating opensim libs"
+rm -rf ./lib
+mkdir ./lib
 cp /usr/lib/libsim.* ./lib/
+rm ./lib/libsim.a
 
+echo "updating opensim-gtk"
 cp $SRC_DIR/src/opensim-gtk ./opensim-gtk.py
-cp $SRC_DIR/opensim-main.glade ./opensim-main.glade
+cp $SRC_DIR/src/opensim-main.glade ./opensim-main.glade
 
+echo "resetting app privledges"
+cd ../../..
 sudo chown -R $USER:$GROUP Model.app
