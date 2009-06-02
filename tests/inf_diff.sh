0ff011d50f57c4e2cc7ef9e84720d695fb82a582
Bobby Powers <bobby@laptop.org>
2009-06-02 01:58:09 -0400

    [build] make dist fixes

--- tests/inf_diff.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-opensim ../examples/infection.osm -t python > infection_c.py
-opensim-d ../examples/infection.osm -t python > infection_g.py
-diff -u infection_c.py infection_g.py > infection.diff
-rm infection_*.py
-#gedit infection.diff &
